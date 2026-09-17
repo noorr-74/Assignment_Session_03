89e27852b0d4afa30cd6a4f6163f516ad8bc10b0
noorr-74 <nourhanyhanafy2005@gmail.com>
2026-09-18 01:53:13 +0300

    Submit rest of assignment

--- Assignment 03/Program.cs
+++ Assignment 03/Program.cs
@@ -290,6 +290,331 @@ class program
         }
         #endregion
 
+        #region Q16
+        void Q16()
+        {
+            Console.Write("Enter a number to convert: ");
+            int decNum = Convert.ToInt32(Console.ReadLine());
+
+            string binaryStr = "";
+            int tempDec = decNum;
+
+            if (tempDec == 0)
+            {
+                binaryStr = "0";
+            }
+
+            while (tempDec > 0)
+            {
+                binaryStr = (tempDec % 2) + binaryStr;
+                tempDec /= 2;
+            }
+
+            Console.WriteLine("The Binary of " + decNum + " is " + binaryStr);
+        }
+        #endregion
+
+        #region Q17
+        void Q17()
+        {
+            Console.Write("Enter x1 y1: ");
+            string[] point1 = Console.ReadLine().Split(' ');
+
+            int x1 = Convert.ToInt32(point1[0]);
+            int y1 = Convert.ToInt32(point1[1]);
+
+            Console.Write("Enter x2 y2: ");
+            string[] point2 = Console.ReadLine().Split(' ');
+
+            int x2 = Convert.ToInt32(point2[0]);
+            int y2 = Convert.ToInt32(point2[1]);
+
+            Console.Write("Enter x3 y3: ");
+            string[] point3 = Console.ReadLine().Split(' ');
+
+            int x3 = Convert.ToInt32(point3[0]);
+            int y3 = Convert.ToInt32(point3[1]);
+
+            if ((y2 - y1) * (x3 - x2) == (y3 - y2) * (x2 - x1))
+            {
+                Console.WriteLine("Points lie on a single straight line.");
+            }
+            else
+            {
+                Console.WriteLine("Points do not lie on a single straight line.");
+            }
+        }
+        #endregion
+
+        #region Q18
+        void Q18()
+        {
+            Console.Write("Enter time taken in hours: ");
+            double hours = Convert.ToDouble(Console.ReadLine());
+
+            if (hours >= 2 && hours <= 3)
+            {
+                Console.WriteLine("Highly efficient");
+            }
+            else if (hours > 3 && hours <= 4)
+            {
+                Console.WriteLine("Instructed to increase speed");
+            }
+            else if (hours > 4 && hours <= 5)
+            {
+                Console.WriteLine("Provided with training to enhance speed");
+            }
+            else if (hours > 5)
+            {
+                Console.WriteLine("Required to leave the company");
+            }
+            else
+            {
+                Console.WriteLine("Invalid time entered");
+            }
+        }
+        #endregion
+
+        #region Q19
+        void Q19()
+        {
+            Console.Write("Enter size n: ");
+            int n19 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n19; i++)
+            {
+                for (int j = 0; j < n19; j++)
+                {
+                    if (i == j)
+                        Console.Write("1 ");
+                    else
+                        Console.Write("0 ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        #endregion
+
+        #region Q20
+        void Q20()
+        {
+            int[] arr20 = { 1, 2, 3, 4, 5 };
+            int sum20 = 0;
+
+            for (int i = 0; i < arr20.Length; i++)
+            {
+                sum20 += arr20[i];
+            }
+
+            Console.WriteLine("Sum of all elements = " + sum20);
+        }
+        #endregion
+
+        #region Q21
+        void Q21()
+        {
+            int[] arrA = { 1, 3, 5 };
+            int[] arrB = { 2, 4, 6 };
+
+            int[] merged = new int[arrA.Length + arrB.Length];
+
+            for (int i = 0; i < arrA.Length; i++)
+            {
+                merged[i] = arrA[i];
+            }
+
+            for (int i = 0; i < arrB.Length; i++)
+            {
+                merged[arrA.Length + i] = arrB[i];
+            }
+
+            Array.Sort(merged);
+
+            Console.Write("Merged and sorted array: ");
+
+            for (int i = 0; i < merged.Length; i++)
+            {
+                Console.Write(merged[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+        #endregion
+
+        #region Q22
+        void Q22()
+        {
+            int[] arr22 = { 1, 2, 2, 3, 1, 4 };
+            bool[] visited = new bool[arr22.Length];
+
+            for (int i = 0; i < arr22.Length; i++)
+            {
+                if (visited[i])
+                    continue;
+
+                int count = 1;
+
+                for (int j = i + 1; j < arr22.Length; j++)
+                {
+                    if (arr22[i] == arr22[j])
+                    {
+                        visited[j] = true;
+                        count++;
+                    }
+                }
+
+                Console.WriteLine(arr22[i] + " occurs " + count + " times");
+            }
+        }
+        #endregion
+
+        #region Q23
+        void Q23()
+        {
+            int[] arr23 = { 5, 2, 9, 1, 7 };
+
+            int max23 = arr23[0];
+            int min23 = arr23[0];
+
+            for (int i = 1; i < arr23.Length; i++)
+            {
+                if (arr23[i] > max23)
+                    max23 = arr23[i];
+
+                if (arr23[i] < min23)
+                    min23 = arr23[i];
+            }
+
+            Console.WriteLine("Maximum element = " + max23);
+            Console.WriteLine("Minimum element = " + min23);
+        }
+        #endregion
+
+        #region Q24
+        void Q24()
+        {
+            int[] arr24 = { 12, 35, 1, 10, 34, 1 };
+
+            int largest = int.MinValue;
+            int secondLargest = int.MinValue;
+
+            for (int i = 0; i < arr24.Length; i++)
+            {
+                if (arr24[i] > largest)
+                {
+                    secondLargest = largest;
+                    largest = arr24[i];
+                }
+                else if (arr24[i] > secondLargest && arr24[i] != largest)
+                {
+                    secondLargest = arr24[i];
+                }
+            }
+
+            Console.WriteLine("Second largest element = " + secondLargest);
+        }
+        #endregion
+
+        #region Q25
+        void Q25()
+        {
+            Console.Write("Enter size of array: ");
+            int size25 = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr25 = new int[size25];
+
+            Console.WriteLine("Enter array elements:");
+
+            for (int i = 0; i < size25; i++)
+            {
+                arr25[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int maxDist = -1;
+
+            for (int i = 0; i < size25; i++)
+            {
+                for (int j = size25 - 1; j > i; j--)
+                {
+                    if (arr25[i] == arr25[j])
+                    {
+                        int dist = j - i - 1;
+
+                        if (dist > maxDist)
+                        {
+                            maxDist = dist;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Longest distance = " + maxDist);
+        }
+        #endregion
+
+        #region Q26
+        void Q26()
+        {
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');
+
+            Array.Reverse(words);
+
+            Console.WriteLine(string.Join(" ", words));
+        }
+        #endregion
+
+        #region Q27
+        void Q27()
+        {
+            int[,] matrix1 = new int[2, 2];
+            int[,] matrix2 = new int[2, 2];
+
+            Console.WriteLine("Enter 4 elements for 2x2 matrix:");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matrix1[i, j] = Convert.ToInt32(Console.ReadLine());
+                    matrix2[i, j] = matrix1[i, j];
+                }
+            }
+
+            Console.WriteLine("Second Array Elements:");
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(matrix2[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        #endregion
+
+        #region Q28
+        void Q28()
+        {
+            int[] arr28 = { 1, 2, 3, 4, 5 };
+
+            Console.Write("Array in reverse order: ");
+
+            for (int i = arr28.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr28[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+        #endregion
     }
 }
 
